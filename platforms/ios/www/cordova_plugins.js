7fe0de5b96954be54b6ef44649f0a7c5dd5b23ea
aliirz <dejavu87@gmail.com>
2015-05-10 12:55:14 +0500

    pulling latest code

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
-        "id": "org.apache.cordova.statusbar.statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
         "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
         "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
         "clobbers": [
@@ -41,14 +34,29 @@ module.exports = [
         "clobbers": [
             "CameraPopoverHandle"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.cloudsky.cordovaplugins.zbar/www/zbar.js",
+        "id": "org.cloudsky.cordovaplugins.zbar.zBar",
+        "clobbers": [
+            "cloudSky.zBar"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.statusbar": "0.1.10",
     "com.phonegap.plugins.barcodescanner": "2.0.1",
-    "org.apache.cordova.camera": "0.3.6"
+    "org.apache.cordova.camera": "0.3.6",
+    "org.apache.cordova.statusbar": "0.1.10",
+    "org.cloudsky.cordovaplugins.zbar": "1.3.0"
 }
 // BOTTOM OF METADATA
 });
