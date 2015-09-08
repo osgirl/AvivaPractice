d0f206b75ed9741e080651a53e13ea0bd295a33c
Ali Raza <ali@sweetpixelstudios.com>
2015-09-08 20:15:02 +0500

    gulp cordova cli support

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
 'use strict';
 
-var gulp = require('gulp'),
-    gls  = require('gulp-live-server');
+var gulp    = require('gulp'),
+    gls     = require('gulp-live-server'),
+    cordova = require('gulp-cordova');
 
 
+
+// serve via local web server
 gulp.task('serve', function() {
   var server = gls.static(['www']);
   server.start();
 });
+
+
+//build iOS project
+gulp.task('ios', function() {
+  gulp.src('./cordovaconfig.json')
+    .pipe(cordova());
+});
+
+
+//build iOS project
+// gulp.task('android', function() {
+//   gulp.src('./cordovaconfig.json')
+//     .pipe(cordova(['build android']));
+// });
